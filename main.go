80169cab36eae0a0db40ba1ccb6957ec3408c4d1
bs255200@163.com <bs255200@163.com>
2021-02-15 21:27:31 +0800

    add

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default() //返回默认的路由引擎
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello golang",
+		})
+	})
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "GET",
+		})
+	})
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "POST",
+		})
+	})
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "PUT",
+		})
+	})
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "DELETE",
 		})
 	})
 	r.Run()
