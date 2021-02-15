b85199c3e5c27dc2fdd19db42d6cfb6aee307b6c
bs255200@163.com <bs255200@163.com>
2021-02-15 20:08:33 +0800

    add

--- demo.go
+++ demo.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>hello is go web test</h1>")
+}
+
+func main() {
+	http.HandleFunc("/hello", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println("http server faild,err:", err)
+		return
+	}
 }
