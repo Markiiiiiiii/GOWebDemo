4d77573048cff7523a6f828ff8f067aac42b4f5b
Markiiiiiiii <bs255200@163.com>
2021-02-19 10:48:32 +0800

    ADD

--- 7/demo.go
+++ 7/demo.go
@@ -16,6 +16,19 @@ func main() {
 		name := "LLL"
 		t.Execute(w, name)
 	})
+	// http.HandleFunc("/xcc", func(w http.ResponseWriter, r *http.Request) {
+	// 	t, err := template.New("index.tpl").Funcs(template.FuncMap{
+	// 		"safe": func(s string) template.HTML {
+	// 			return template.HTML(s)
+	// 		},
+	// 	}).ParseFiles("./index.tpl")
+	// 	if err != nil {
+	// 		fmt.Println("err:", err)
+	// 		return
+	// 	}
+	// 	name := "LLL"
+	// 	t.Execute(w, name)
+	// })
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
 		fmt.Println("err:", err)
